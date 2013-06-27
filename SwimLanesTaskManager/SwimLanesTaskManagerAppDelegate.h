aef191c78e45e9598d3d78d654f75737ea0244d7
John Pavley <john@pavley.com>
2013-06-26 22:25:41 -0400

    added ivars, consts, and data source methods

--- SwimLanesTaskManager/SwimLanesTaskManagerAppDelegate.h
+++ SwimLanesTaskManager/SwimLanesTaskManagerAppDelegate.h
@@ -8,7 +8,18 @@
 
 #import <Cocoa/Cocoa.h>
 
+FOUNDATION_EXPORT NSString *const kColumn1;
+FOUNDATION_EXPORT NSString *const kColumn2;
+FOUNDATION_EXPORT NSString *const kColumn3;
+FOUNDATION_EXPORT NSString *const kColumn4;
+
+
 @interface SwimLanesTaskManagerAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (strong) NSArray *_backlog;
+@property (strong) NSArray *_inProgress;
+@property (strong) NSArray *_completed;
+@property (strong) NSArray *_onHold;
 
 @property (assign) IBOutlet NSWindow *window;
 @property (weak) IBOutlet NSTextField *taskTextField;
@@ -16,6 +27,7 @@
 @property (weak) IBOutlet NSButton *clearCompletedButton;
 @property (weak) IBOutlet NSButton *clearOnHoldButton;
 @property (weak) IBOutlet NSTableView *taskTable;
+
 - (IBAction)addTask:(id)sender;
 - (IBAction)clearCompleted:(id)sender;
 - (IBAction)clearOnHold:(id)sender;
--- SwimLanesTaskManager/SwimLanesTaskManagerAppDelegate.m
+++ SwimLanesTaskManager/SwimLanesTaskManagerAppDelegate.m
@@ -8,19 +8,86 @@
 
 #import "SwimLanesTaskManagerAppDelegate.h"
 
+NSString *const kColumn1 = @"col1";
+NSString *const kColumn2 = @"col2";
+NSString *const kColumn3 = @"col3";
+NSString *const kColumn4 = @"col4";
+
 @implementation SwimLanesTaskManagerAppDelegate
+
+@synthesize _backlog,
+            _inProgress,
+            _completed,
+            _onHold;
+
+@synthesize window,
+            taskTextField,
+            taskAddButton,
+            taskTable,
+            clearCompletedButton,
+            clearOnHoldButton;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
 }
 
-- (IBAction)addTask:(id)sender {
+- (IBAction)addTask:(id)sender
+{
+    
 }
 
-- (IBAction)clearCompleted:(id)sender {
+- (IBAction)clearCompleted:(id)sender
+{
+    
 }
 
-- (IBAction)clearOnHold:(id)sender {
+- (IBAction)clearOnHold:(id)sender
+{
+    
 }
+
+- (void)awakeFromNib
+{
+    
+}
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tv
+{
+    NSInteger result = 0;
+    
+    // return the lenth of the array with the most members
+    
+    result = MAX(result, [_backlog count]);
+    result = MAX(result, [_inProgress count]);
+    result = MAX(result, [_completed count]);
+    result = MAX(result, [_onHold count]);
+    
+    return result;
+}
+
+- (id)tableView:(NSTableView *)tv objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
+{
+    NSString *result = nil;
+    
+    if ([[tableColumn identifier] isEqualToString:kColumn1]) {
+        result = [_backlog objectAtIndex:row];
+    } else if ([[tableColumn identifier] isEqualToString:kColumn2]) {
+        result = [_inProgress objectAtIndex:row];
+        
+    } else if ([[tableColumn identifier] isEqualToString:kColumn3]) {
+        result = [_completed objectAtIndex:row];
+       
+    } else if ([[tableColumn identifier] isEqualToString:kColumn4]) {
+        result = [_onHold objectAtIndex:row];
+    }
+    
+    return result;
+}
+
+- (void)tableViewSelectionDidChange:(NSNotification *)notification
+{
+    
+}
+
 @end
